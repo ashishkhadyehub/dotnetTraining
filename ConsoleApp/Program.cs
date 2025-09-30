0b3707558cae8398648e9488a3f5d920a1e25d96
ashishkhadye33 <ashishkhadye33@gmail.com>
2025-09-30 12:26:36 +0530

    Add project files.

---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Enter you Name: ");
+Console.WriteLine("---------");
+//to get input from user
+string name = Console.ReadLine();
+Console.WriteLine($"Hello {name}");
+
+
+
+//let, const, var
+int a = 10;
+int b = 80;
+Console.WriteLine(a + b);
+
+
+
+

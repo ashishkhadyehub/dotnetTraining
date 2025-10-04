405d207314c7ed965f1f702e87026f635752cb29
ashishkhadye33 <ashishkhadye33@gmail.com>
2025-10-04 11:50:46 +0530

    csharp session 3 and 4

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -13,6 +13,10 @@
 //int b = 80;
 //Console.WriteLine(a + b);
 
+
+
+using ConsoleApp;
+
 int myInt = 9;
 double myDouble = myInt;
 
@@ -96,3 +100,62 @@ switch (day)
 }
 // Outputs "Thursday" (day 4)
 
+
+
+//int i = 0;
+//while (i < 5)
+//{
+//    Console.WriteLine(i);
+//    i++;
+//}
+
+//for (int i = 0; i < 5; i++)
+//{
+//    Console.WriteLine(i);
+//}
+
+int i = 9;
+do
+{
+    Console.WriteLine(i);
+    i++;
+}
+while (i < 5);
+
+// let arr = [20,30,90]
+
+
+string[] cars = {"Volvo", "BMW", "Ford", "Mazda" };
+Console.WriteLine(cars.Length);
+Console.WriteLine(cars[0]);
+Array.Sort(cars);
+foreach (string car in cars)
+{
+    Console.WriteLine(car);
+}
+
+int[] myNumbers = { 5, 1, 8, 9,90,100 };
+Console.WriteLine(myNumbers.Max());  // returns the largest value
+Console.WriteLine(myNumbers.Min());  // returns the smallest value
+Console.WriteLine(myNumbers.Sum());  // returns the sum of elements
+
+
+void Mymethod()
+{
+    Console.WriteLine("I just got executed!");
+}
+
+Mymethod();
+
+void greeting(string name)
+{
+    Console.WriteLine($"Hello, {name}");
+    //Console.WriteLine("Hello, " + name);
+}
+
+greeting("Shree");
+greeting("Test");
+
+Method_Overloading obj = new Method_Overloading();
+Console.WriteLine(obj.PlusMethod(2,8));
+Console.WriteLine(obj.PlusMethod(20.90,89.90));
